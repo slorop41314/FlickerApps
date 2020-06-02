6e10a5bf27d0baf917a133fd778d25f8c103a2be
Albert Stanley <albertstanleyy@gmail.com>
2020-06-02 22:48:19 +0700

    Add font and splash screen

---
+++ src/containers/Splash/index.js
@@ -0,0 +1,32 @@
+import React, { Component, useEffect } from 'react'
+
+import { View, StyleSheet } from 'react-native'
+import { Image } from '../../components/Image'
+import { Constant } from '../../utils/constant'
+
+const Splash = ({ navigation }) => {
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.replace(Constant.HOME_ROUTE_NAME)
+        }, 2000);
+    })
+    return (
+        <View style={styles.container}>
+            <Image style={styles.logo} source={require("../../assets/images/logo.png")} />
+        </View>
+    )
+}
+export default Splash
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    logo: {
+        height: 150,
+        width: 150,
+        resizeMode: "contain"
+    }
+})
